c9a4a64d6adda10ccf1397454453a47f877f0378
Tom <thomas.spencer@codurance.com>
2020-07-02 08:12:38 +0100

    Add todo

--- src/main/java/org/openchat/domain/posts/PostService.java
+++ src/main/java/org/openchat/domain/posts/PostService.java
@@ -2,6 +2,7 @@ package org.openchat.domain.posts;
 
 public class PostService {
 
+  // TODO 20:16 Episode 3: UnsupportedOperationException fail for IT_TimelineAPI
   public Post createPost(String userId, String postText) throws InappropriateLanguageException {
     throw new UnsupportedOperationException();
   }
